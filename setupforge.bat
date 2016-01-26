62fecda25ffa82c92c8fc6e1d5415d6dabc6251f
The_Fireplace <the.f1repl4ce@gmail.com>
2016-01-26 08:23:07 -0600

    Initial Commit

---
+++ setupforge.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+TITLE The_Fireplace's Forge Tools - Setup v1.6
+call gradlew setupDecompWorkspace idea
+ECHO ****************************
+ECHO Forge idea workspace setup complete!
+ECHO ****************************
+PAUSE
