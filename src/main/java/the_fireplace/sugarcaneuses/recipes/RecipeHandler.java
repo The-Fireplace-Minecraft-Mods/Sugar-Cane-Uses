3eb5d3eb713cf0dd246bfdb47d18ba8ed4c5ceaa
SirBlobman <blobmansrealm@gmail.com>
2016-01-26 18:17:20 -0500

    Changed some things
    Added myself and changed something to static. I also created two
    itemstacks that will fir in the recipe

--- src/main/java/the_fireplace/sugarcaneuses/recipes/RecipeHandler.java
+++ src/main/java/the_fireplace/sugarcaneuses/recipes/RecipeHandler.java
@@ -6,21 +6,24 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 
 /**
- * @author The_Fireplace
+ * int Author = The_Fireplace + SirBlobman;
  */
 public class RecipeHandler {
 	//Put ItemStacks to be used in recipes here. An ItemStack with Sugar Cane is below as an example.
 	static ItemStack sugarCaneStack = new ItemStack(Items.reeds);
+	static ItemStack sugarStack = new ItemStack(Items.sugar);
+	static ItemStack glueStack = new ItemStack(Items.slime_ball);
 
 	/**
 	 * Register recipes here. The first line will be a commented out example.
 	 */
 	public static void registerRecipes(){
-		//addRecipe(sugarCaneStack, "s", "g", "s", 's', sugarStack, 'g', glueStack);
+		addRecipe(sugarCaneStack, "s", "g", "s", 's', sugarStack, 'g', glueStack);
 
 	}
 
-	private void addRecipe(ItemStack output, Object... inputs){
+	//Changed to static to fix error -- SirBlobman
+	private static void addRecipe(ItemStack output, Object... inputs){
 		GameRegistry.addRecipe(new ShapedOreRecipe(output, inputs));
 	}
 }
