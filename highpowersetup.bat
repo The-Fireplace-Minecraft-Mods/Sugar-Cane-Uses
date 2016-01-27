f5c47ec19f6c67829492ac8b155660c92e2444c6
The_Fireplace <the.f1repl4ce@gmail.com>
2016-01-27 12:15:27 -0600

    removed high powered setup

--- highpowersetup.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-TITLE The_Fireplace's Forge Tools - High Allocation Setup v1.0
-call gradlew -DXms3g -DXmx6g setupDecompWorkspace idea
-ECHO ****************************
-ECHO Forge idea workspace setup complete!
-ECHO ****************************
-PAUSE
